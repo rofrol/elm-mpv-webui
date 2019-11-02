079c0974d81b136a49a9ee4f663b7e8ee43db2b5
Roman Frołow <rofrol@gmail.com>
2019-11-02 18:43:54 +0100

    play/pause button

--- webui-page/src/Main.elm
+++ webui-page/src/Main.elm
@@ -2,28 +2,22 @@ module Main exposing (..)
 
 import Browser
 import Element exposing (..)
+import Element.Input as Input
 import Http
 import Json.Decode as D
 
 
 type Msg
     = Sent (Result Http.Error D.Value)
+    | PlayPause
 
 
 main : Program () () Msg
 main =
     Browser.document
-        { init =
-            \_ ->
-                ( ()
-                , Http.post
-                    { url = "http://192.168.0.10:8080/api/toggle_pause"
-                    , body = Http.emptyBody
-                    , expect = Http.expectJson Sent D.value
-                    }
-                )
+        { init = \_ -> ( (), Cmd.none )
         , view = view
-        , update = \_ _ -> ( (), Cmd.none )
+        , update = update
         , subscriptions = \_ -> Sub.none
         }
 
@@ -31,5 +25,31 @@ main =
 view _ =
     { title = "Title"
     , body =
-        [ Element.layout [] (text "Hello") ]
+        [ Element.layout []
+            (column []
+                [ text "Hello"
+                , Input.button []
+                    { onPress = Just PlayPause
+                    , label = text "play/pause"
+                    }
+                ]
+            )
+        ]
     }
+
+
+update msg model =
+    case msg of
+        Sent _ ->
+            ( model, Cmd.none )
+
+        PlayPause ->
+            ( model, playPause )
+
+
+playPause =
+    Http.post
+        { url = "http://192.168.0.10:8080/api/toggle_pause"
+        , body = Http.emptyBody
+        , expect = Http.expectJson Sent D.value
+        }
