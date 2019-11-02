f648ad7a83c345bf9ae6f5b3a9a1c360d7f08282
Roman Frołow <rofrol@gmail.com>
2019-11-02 18:36:10 +0100

    toggle_pause

--- webui-page/src/Main.elm
+++ webui-page/src/Main.elm
@@ -2,12 +2,26 @@ module Main exposing (..)
 
 import Browser
 import Element exposing (..)
+import Http
+import Json.Decode as D
 
 
-main : Program () () msg
+type Msg
+    = Sent (Result Http.Error D.Value)
+
+
+main : Program () () Msg
 main =
     Browser.document
-        { init = \_ -> ( (), Cmd.none )
+        { init =
+            \_ ->
+                ( ()
+                , Http.post
+                    { url = "http://192.168.0.10:8080/api/toggle_pause"
+                    , body = Http.emptyBody
+                    , expect = Http.expectJson Sent D.value
+                    }
+                )
         , view = view
         , update = \_ _ -> ( (), Cmd.none )
         , subscriptions = \_ -> Sub.none
